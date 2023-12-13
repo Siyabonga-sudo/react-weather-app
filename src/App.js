75af7b4854fce29d0168e368b0077e3ec84f8d69
Siyabonga Msipa <siyabongamsipa00@gmail.com>
2023-12-13 17:46:43 +0200

    Added bootsrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import Weather from './Weather';
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
+      <Weather />
+      <footer>
+        This project was coded by Siyabonga Msipa and is {""} <a href='https://github.com/Siyabonga-sudo/react-weather-app' target="_blank" rel="noreferrer" >open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,33 @@
 import React from "react";
 
+import './weather.css';
+
 export default function Weather() {
     return (
         <div className="Weather">
-            <h1>Vereeniging</h1>
+            <div className="container">
             
+            <h1>Vereeniging</h1>
+            <ul>
+        
+            <li>Saturday 08:00</li>
+            <li>Sunny</li>
+            </ul>
+            <div className="row">
+            <div className="col-6">
             <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny"/>
             <h2>24C</h2>
+            </div>
+            <div className="col-6">
             <ul>
                 <li>Precipitation: 0%</li>
                 <li>Humidity: 35%</li>
                 <li>Wind: 13 km/h</li>
             </ul>
-            <ul>
-                <li>Weather</li>
-                <li>Saturday 08:00</li>
-                <li>Sunny</li>
-            </ul>
+            </div>
+            
+            </div>
+            </div>
             
         </div>
     )
