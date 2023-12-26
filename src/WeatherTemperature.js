d574d66eb6584009190a769355d126f2c6c21614
Siyabonga Msipa <siyabongamsipa00@gmail.com>
2023-12-26 22:09:20 +0200

    Removed Fahrenheit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celcius")
-
-  function showCelcius(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  if (unit === "celcius") {
     return (
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit">
-          °C | {" "}
-          <a href="/"  onClick={showFahrenheit}>
-            °F
-          </a>
+          °C 
         </span>
-        
-
       </div>
     );
-  } 
-  else {
-    let fahrenheit = (props.celsius * 9/5 ) + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <a href="/" onClick={showCelcius}>
-            °C
-          </a> {" "}
-          | °F
-        </span>
-      </div>
-    )
-  }
 }
